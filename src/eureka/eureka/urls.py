03a5cbb8fbee12651de4a04b8726c4df649af098
Thomas Herman <hermanthomas9@outlook.com>
2016-05-09 21:34:28 +0200

    [add] Bootstrap to project. Check html header to see how to import it in a HTML file

--- src/eureka/eureka/urls.py
+++ src/eureka/eureka/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
 	url(r'^login/', include("login.urls")),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^establishments/', include("establishments.urls")),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
