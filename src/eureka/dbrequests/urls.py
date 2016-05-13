5fb3e346e610334fdeeceb5349ed1c6dc135b153
Thomas Herman <hermanthomas9@outlook.com>
2016-05-13 20:32:30 +0200

    [add] tag request + R6

--- src/eureka/dbrequests/urls.py
+++ src/eureka/dbrequests/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
 	# ex: /requests/establishments/4
     url(r'^establishments/(?P<request_id>[0-9]+)/$', views.establishments_request_detail, name='establishment_request_detail'),
     url(r'^users/(?P<request_id>[0-9]+)/$', views.users_request_detail, name='users_request_detail'),
+    url(r'^tags/(?P<request_id>[0-9]+)/$', views.tag_request_detail, name='tag_request_detail'),
 ]
